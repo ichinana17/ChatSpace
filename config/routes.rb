3bc25f2b04dfebe0a15c6e1dcb3be2ea631adcc2
ichinana17 <ishiinaoki@ishiinaokinoMacBook-puro.local>
2019-01-22 21:50:31 +0900

    グループ機能のルーティング設定
    新規作成画面を表示するnew、新規作成されたグループを保存するcreate、編集画面を表示するedit、編集されたグループを更新する:updat
    eを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-  resources :users, only: [:edit, :update]
+  root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :creata, :edit, :update] do
+    resources :messages, only: [:index, :create]
 end
